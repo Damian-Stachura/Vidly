f7196ebea6d9ec0d1b09b493aa75bc92912f72d0
Damian Stachura <damian.stachura@o2.pl>
2018-10-01 15:32:27 +0200

    Adding code-first migrations, creating database, adding form for creating new customers, updating existing models, deleted old (trial) code

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        public int NumberInStock { get; set; }
+    }
+}
